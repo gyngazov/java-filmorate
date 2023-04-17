f8d100dea4b0eb1c2193ad944150b470a74972cf
Leo <l.gyngazov@yandex.ru>
2023-04-17 11:22:26 +0300

    fix: перенести запятые 4

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -23,29 +23,23 @@ class FilmorateApplicationTests {
 		String emptyName = "";
 		final ValidationException exception1 = assertThrows(ValidationException.class,
 				() -> filmController.createFilm(new Film(12, emptyName, "desc",
-						LocalDate.of(2012, 12, 12), 12))
-		);
+						LocalDate.of(2012, 12, 12), 12)));
 		LocalDate older = LocalDate.of(1895, 12, 28).minusDays(1);
 		final ValidationException exception2 = assertThrows(ValidationException.class,
-				() -> filmController.createFilm(new Film(12, "name", "desc",
-						older, 12))
-		);
+				() -> filmController.createFilm(new Film(12, "name", "desc", older, 12)));
 		String longDesc = "w".repeat(201);
 		final ValidationException exception3 = assertThrows(ValidationException.class,
 				() -> filmController.createFilm(new Film(12, "name", longDesc,
-						LocalDate.of(2012, 12, 12), 12))
-		);
+						LocalDate.of(2012, 12, 12), 12)));
 		int negativeDuration = -3;
 		final ValidationException exception4 = assertThrows(ValidationException.class,
 				() -> filmController.createFilm(new Film(12, "name", "dfr",
-						LocalDate.of(2012, 12, 12), negativeDuration))
-		);
+						LocalDate.of(2012, 12, 12), negativeDuration)));
 		assertAll(
 			() -> assertEquals("Наименование фильма не может быть пустым.", exception1.getMessage()),
 			() -> assertEquals("Слишком старая дата фильма.", exception2.getMessage()),
 			() -> assertEquals("Максимальная длина описания — 200 символов.", exception3.getMessage()),
-			() -> assertEquals("Продолжительность фильма должна быть положительной.", exception4.getMessage())
-		);
+			() -> assertEquals("Продолжительность фильма должна быть положительной.", exception4.getMessage()));
 	}
 
 }
