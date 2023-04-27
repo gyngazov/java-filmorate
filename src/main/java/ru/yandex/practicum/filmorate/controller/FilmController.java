1ce6e953d146d9a83b6cba47a60f98328a321490
Leo <l.gyngazov@yandex.ru>
2023-04-27 19:11:08 +0300

    style: разделить пустой строкой 4

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -8,13 +8,13 @@ import ru.yandex.practicum.filmorate.model.ValidationException;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
 import java.util.*;
 
 @RestController
 @RequestMapping("/films")
 public class FilmController {
     private final FilmService filmService;
+
     @Autowired
     public FilmController(FilmService filmService) {
         this.filmService = filmService;
@@ -24,6 +24,7 @@ public class FilmController {
     public Collection<Film> getFilms() {
         return filmService.getFilms();
     }
+
     @GetMapping("/{id}")
     public Film getFilm(@PathVariable int id) throws ObjectNotFoundException {
         return filmService.getFilm(id);
@@ -39,14 +40,17 @@ public class FilmController {
             throws ValidationException, ObjectNotFoundException {
         return filmService.updateFilm(film);
     }
+
     @PutMapping("/{id}/like/{userId}")
     public void addLike(@PathVariable int id, @PathVariable int userId) throws ObjectNotFoundException {
         filmService.addLike(id, userId);
     }
+
     @DeleteMapping("/{id}/like/{userId}")
     public void deleteLike(@PathVariable int id, @PathVariable int userId) throws ObjectNotFoundException {
         filmService.deleteLike(id, userId);
     }
+
     @GetMapping("/popular")
     public List<Film> getPopularFilms(@RequestParam(defaultValue = "10") int count) {
         return filmService.getFilmsByPopularity(count);
