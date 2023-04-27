406e5d0626ddc2cdfbb0bf1ddf9d78c4877b6a4b
Leo <l.gyngazov@yandex.ru>
2023-04-27 19:05:13 +0300

    style: разделить пустой строкой 2

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -11,18 +11,21 @@ import ru.yandex.practicum.filmorate.model.ValidationException;
 public class ErrorHandler {
 
     @ExceptionHandler(ValidationException.class)
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
 
     public ExceptionResponse handleValidationError(final Exception e) {
         return new ExceptionResponse("Ошибка валидации", e.getMessage());
     }
     @ExceptionHandler(ObjectNotFoundException.class)
+
     @ResponseStatus(HttpStatus.NOT_FOUND)
 
     public ExceptionResponse handleAbsentObject(final Exception e) {
         return new ExceptionResponse("Не найден объект", e.getMessage());
     }
     @ExceptionHandler(Exception.class)
+
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 
     public ExceptionResponse handleException(final Throwable t) {
