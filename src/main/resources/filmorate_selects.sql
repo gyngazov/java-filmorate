66952a1101ba5aa8dc4dc40c17c049a23eb45a53
gyngazov <118831197+gyngazov@users.noreply.github.com>
2023-05-03 14:01:31 +0300

    Изменить имена, добавить is_accepted.
    Изменить имена таблиц, добавить is_accepted = true для общих друзей.

--- src/main/resources/filmorate_selects.sql
+++ src/main/resources/filmorate_selects.sql
@@ -1,6 +1,6 @@
 -- 10 самых популярных фильмов
 select f.name
-from film f
+from films f
 inner join likes l on
 l.film_id=f.id
 group by f.name
@@ -9,9 +9,12 @@ limit 10;
 
 -- общие друзья у двух пользователей с id 7 и 17
 select u.name
-from friend f1
-inner join friend f2 on
+from friends f1
+inner join friends f2 on
 f1.friend_id=f2.friend_id
 inner join users u on 
 u.id=f1.friend_id
-where f1.user_id=7 and f2.user_id=17
+where f1.user_id=7 
+and f1.is_accepted = true
+and f2.user_id=17 
+and f2.is_accepted = true
